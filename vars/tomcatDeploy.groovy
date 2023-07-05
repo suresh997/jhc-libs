a0ca09762f0627ce3f10fae26c0f65242ad80089
suresh997 <44109753+suresh997@users.noreply.github.com>
2023-07-05 17:14:44 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,8 +1,8 @@
 def call(ip,creds,warFile){
   //install ssh agent
-                sshagent({creds]) {
+                sshagent([creds]) {
     // copy war to dev 
-    sh"scp target/${warFile} ec2-user@${ip}:/opt/tomcat9/webapps"
+    sh"scp -o StrictHostKeyChecking=no target/${warFile} ec2-user@${ip}:/opt/tomcat9/webapps"
     // restart tomcat
     sh"ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
     sh"ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
